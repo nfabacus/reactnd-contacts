d2d5fc60560cd5df63b8dea5d4d98facb1c614ca
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-07-29 05:19:33 +0100

    solved the form submit issue. it was a typo.

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -6,16 +6,16 @@ import serializeform from 'form-serialize'
 class CreateContact extends Component {
   handleSubmit = (e)=>{
     e.preventDefault()
-    // const values = serializeform(e.target, { hash: true })
-    // console.log("e.target: ", e.target)
-    // console.log("serialized values: ", values)
+    const values = serializeform(e.target, { hash: true })
+    console.log("e.target: ", e.target)
+    console.log("serialized values: ", values)
 
   }
   render(){
     return (
       <div>
         <Link className="close-create-contact" to="/">Close</Link>
-        <form pnSubmit={this.handleSubmit} className="create-contact-form">
+        <form onSubmit={this.handleSubmit} className="create-contact-form">
           <ImageInput
             className="create-contact-avatar-input"
             name="avatarURL"
